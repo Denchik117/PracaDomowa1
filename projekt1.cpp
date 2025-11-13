2587e5507453219bfb3ec67455590a03b907755d
Denys <denyspshen@gmail.com>
2025-11-13 21:09:12 +0100

    Add option #1 in menu

--- projekt1.cpp
+++ projekt1.cpp
@@ -16,6 +16,8 @@ int main() {
         cout << endl;
         cout << endl;
         cout << "MENU" << endl;
+	cout << "1. Silnia" << endl;
+	if(wyjscie == 1) cout << a << "! = " << silnia(a) << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
         cout << "0. Wyjscie" << endl;
         cin >> wyjscie;
